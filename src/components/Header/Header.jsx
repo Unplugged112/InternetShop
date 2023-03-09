3a8940c1a8f2fc2e208fc5295de5c885f1753d77
Marat <msabitov04@gmail.com>
2023-03-09 15:53:58 +0300

    Create Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./Header.scss"
 function Header() {
   return (
-	 <div>Header</div>
-  )
+    <header className="header">
+      <div className="header__container container">
+        <div className="header__left">
+          <div className="header__logo">Logo</div>
+          <div className="header__filters">
+            <img src="/image/Header/Filters.png" alt="" />
+            <span>Каталог</span>
+          </div>
+        </div>
+        <div className="header__right">
+          <nav className="header__menu menu">
+            <ul className="menu__list">
+              <li>
+                <a href="">Главная</a>{" "}
+              </li>
+              <li>
+                <a href="">О нас</a>{" "}
+              </li>
+              <li>
+                <a href="">FAQ</a>
+              </li>
+              <li>
+                <a href="">Блог</a>
+              </li>
+              <li>
+                <a href="">Акции</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="header__verification">
+            <div className="header__verification-reg">
+              <a href="">Регистрация</a>
+            </div>
+            <div className="header__verification-login">
+              <a href="">Войти</a>
+            </div>
+          </div>
+          <div className="header__basket">
+            <img src="/image/Header/Basket.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
