c99be23615fd104a10a7d2d9efd90946d237aede
Marat <msabitov04@gmail.com>
2023-03-10 21:36:47 +0300

    Add homepage

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+	 <main className="page">
+		<div className="page__container container">
+			<div className="page__slider">
+				
+			</div>
+		</div>
+	 </main>
+  )
+}
+
+export default Homepage
